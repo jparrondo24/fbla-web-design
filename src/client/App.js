bd5dc150c82eb1b3f79c917c8207cb71dfd55e9d
Justin Parrondo <justinparrondo@gmail.com>
2019-12-08 23:55:23 -0500

    Finishes the home page as well as the whole app's navbar and footer

--- src/client/App.js
+++ src/client/App.js
@@ -5,19 +5,34 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './stylesheets/app.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Image from 'react-bootstrap/Image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
+import PlaneLogo from './plane-logo.png';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="app">
         <header>
-          <Navbar fixed="top">
-            <Navbar.Brand href="#home">Sky High Airlines</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Flight Schedules</Nav.Link>
-              <Nav.Link href="#features">High Flyers</Nav.Link>
-              <Nav.Link href="#pricing">Careers</Nav.Link>
-            </Nav>
+          <Navbar collapseOnSelect expand="md" fixed="top">
+            <Navbar.Brand href="#home">
+              <Image 
+                alt="Sky High Airlines Logo"
+                src={PlaneLogo} 
+                className="d-inline-block"
+              /> &nbsp;
+              Sky High Airlines
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Flight Schedules</Nav.Link>
+                <Nav.Link href="#features">High Flyers</Nav.Link>
+                <Nav.Link href="#pricing">Careers</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
         </header>
         <Router>
@@ -27,6 +42,29 @@ export default class App extends React.Component {
             </Route>
           </Switch>
         </Router>
+        <footer>
+          <Image 
+            alt="Sky High Airlines Logo"
+            src={PlaneLogo} 
+            className="mx-auto d-block"
+          />
+          <hr />
+          <h3>Sky High Airlines</h3>
+          <span>
+            <a target="_blank" href="https://www.facebook.com/AmericanAirlines/">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a> &nbsp;
+            <a target="_blank" href="https://twitter.com/AmericanAir">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a> &nbsp;
+            <a target="_blank" href="https://www.instagram.com/americanair/">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a> &nbsp;&nbsp;
+            <a target="_blank" href="https://www.linkedin.com/company/american-airlines/">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </span>
+        </footer>
       </div>
     );
   }
--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -4,6 +4,11 @@ import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThList, faGlobeAmericas, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faTwitter, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
+import objectFitPollyfill from 'objectFitPolyfill';
 
 import BlueSkyVideo from '../videos/blue-sky.mp4';
 import PlaneLogo from '../plane-logo.png';
@@ -12,60 +17,109 @@ import '../stylesheets/home.css';
 export default class Home extends React.Component {
   render() {
     return (
-	  <div className="home">
-	  	<Container fluid>
-	      <video id="landing-video" loop muted autoPlay={true} playsInline={true}>
-		    <source src={BlueSkyVideo} type="video/mp4" />
-			  Video not working
-		  </video>
-				<Row className="align-items-center" id="landing-video-overlay">
-					<Col
-						xs={{span: 8, offset: 2, order: 2}}
-						md={{span: 5, offset: 1, order: 1}}
-						lg={{span: 4, offset: 2}}
+			<div className="home">
+				<Container fluid>
+					<video
+						className="media"
+						id="landing-video" 
+						loop
+						muted
+						autoPlay={true}
+						playsInline={true}
+						data-object-fit="cover"
 					>
-						<Image className="fade-and-zoom-in mx-auto d-block" id="landing-logo" src={PlaneLogo} alt="Sky High Airlines Logo" fluid />
-					</Col>
-					<Col className="fade-in text-center"
-						xs={{span: 8, offset: 2, order: 1}}
-						md={{span: 5, offset: 0, order: 2}}
-						lg={4}
-					>
-						<h1>Sky High Airlines</h1>
-						<h2>Always reaching higher</h2>
-					</Col>
-				</Row>
-	    </Container>
-	    <Container className="home-body">
-	      <Row>
-					<Col>
-						<h1>Come Fly With Us</h1>
-					</Col>
-					<Col>
-						<p>With a variety of flights ready for your time, location, and destination, you won't have any trouble booking a flight with us.</p>
-						<Button>Browse our Flight Schedules</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<h1>The Sky's the Limit</h1>
-					</Col>
-					<Col>
-						<p>Our High Flyers loyalty program makes it our pleasure to keep you reaching for the skies with us.</p>
-						<Button>Browse our Flight Schedules</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<h1>We're Expanding our Crew</h1>
-					</Col>
-					<Col>
-						<p>Interested in a career with us among the clouds? </p>
-						<Button>Browse our Flight Schedules</Button>
-					</Col>
-				</Row>
-	    </Container>
-	  </div>
-	);
+						<source src={BlueSkyVideo} type="video/mp4" />
+						Video not working
+				</video>
+					<Row className="align-items-center" id="landing-video-overlay">
+						<Col
+							xs={{span: 8, offset: 2, order: 2}}
+							md={{span: 5, offset: 1, order: 1}}
+							lg={{span: 4, offset: 2}}
+						>
+							<Image className="fade-and-zoom-in mx-auto d-block" id="landing-logo" src={PlaneLogo} alt="Sky High Airlines Logo" fluid />
+						</Col>
+						<Col className="fade-in text-center"
+							xs={{span: 8, offset: 2, order: 1}}
+							md={{span: 5, offset: 0, order: 2}}
+							lg={4}
+						>
+							<h1>Sky High Airlines</h1>
+							<h2>Always soaring higher</h2>
+						</Col>
+					</Row>
+				</Container>
+				<Container fluid className="home-body">
+					<Row className="no-gutters">
+						<Col
+							sm={12}
+							md={6}
+						>
+							<h1>Come Fly With Us</h1>
+						</Col>
+						<Col
+							sm={12}
+							md={6}
+						>
+							<p>With a variety of flights ready for your time, location, and destination, you won't have any trouble booking a flight with us.</p>
+							<Button variant="light">Browse our Flight Schedules  <FontAwesomeIcon icon={faThList}/></Button>
+						</Col>
+					</Row>
+					<Row className="no-gutters">
+						<Col
+							sm={{span: 12, order: 1}}
+							md={{span: 6, order: 2}}
+						>
+							<h1>The Sky's the Limit</h1>
+						</Col>
+						<Col
+							sm={{span: 12, order: 2}}
+							md={{span: 6, order: 1}}
+						>
+							<p>Our High Flyers loyalty program makes it our pleasure to keep you reaching for the skies with us.</p>
+							<Button variant="light">Discover High Flyers  <FontAwesomeIcon icon={faGlobeAmericas} /></Button>
+						</Col>
+					</Row>
+					<Row className="no-gutters">
+						<Col
+							sm={12}
+							md={6}
+						>
+							<h1>We're Expanding our Crew</h1>
+						</Col>
+						<Col
+							sm={12}
+							md={6}
+						>
+							<p>Interested in a career with us among the clouds? </p>
+							<Button variant="light">See our Oppurtunities  <FontAwesomeIcon icon={faUser} /></Button>
+						</Col>
+					</Row>
+					<div className="home-footer">
+						<Image 
+							alt="Sky High Airlines Logo"
+							src={PlaneLogo} 
+							className="mx-auto d-block"
+						/>
+						<hr />
+						<h3>Sky High Airlines</h3>
+						<span>
+							<a target="_blank" href="https://www.facebook.com/AmericanAirlines/">
+								<FontAwesomeIcon icon={faFacebookF} />
+							</a> &nbsp;
+							<a target="_blank" href="https://twitter.com/AmericanAir">
+								<FontAwesomeIcon icon={faTwitter} />
+							</a> &nbsp;
+							<a target="_blank" href="https://www.instagram.com/americanair/">
+								<FontAwesomeIcon icon={faInstagram} />
+							</a> &nbsp;&nbsp;
+							<a target="_blank" href="https://www.linkedin.com/company/american-airlines/">
+								<FontAwesomeIcon icon={faLinkedin} />
+							</a>
+						</span>
+					</div>
+				</Container>
+			</div>
+		);
   }
 }
